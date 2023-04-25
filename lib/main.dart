f3892dd91a4fdbbdaeff86d27a3d582bba20f088
mariam <mariamelmogy3@gmail.com>
2023-04-25 22:33:20 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:toku_app_project/views/color_view.dart';
+import 'package:toku_app_project/views/family_view.dart';
+import 'package:toku_app_project/views/home.dart';
+import 'package:toku_app_project/views/numbers_view.dart';
+import 'package:toku_app_project/views/phrases_view.dart';
+
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      // theme: ThemeData(),{
+      routes: {
+        HomeView.id: (context) => const HomeView(),
+        NumbersView.id: (context) => const NumbersView(),
+        ColorView.id: (context) => const ColorView(),
+        FamilyView.id: (context) => const FamilyView(),
+        PhrasesView.id: (context) => const PhrasesView(),
+      },
+      // home: HomeView(),
+      initialRoute: HomeView.id,
+    );
+  }
+}
---
+++ lib/models/category_model.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+import '../enums/category_enum.dart';
+
+class CategoryModel {
+  const CategoryModel(
+      {required this.category_type, required this.text, required this.color});
+  final String text;
+  final Color color;
+  final CategoryType category_type;
+}
